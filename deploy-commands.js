7c8416b5addb3b7f1a188c557577669649e8e3f7
Cuan Otto <cuan.otto@gmail.com>
2024-05-16 10:49:06 +0200

    Add guild specifc refresh

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require("discord.js");
-const { clientId, token } = require("./config.json");
+const { clientId, guildId, token } = require("./config.json");
 const fs = require("node:fs");
 const path = require("node:path");
 
@@ -40,7 +40,7 @@ const rest = new REST().setToken(token);
 
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(
-      // Routes.applicationGuildCommands(clientId, guildId),
+      Routes.applicationGuildCommands(clientId, guildId),
       Routes.applicationCommands(clientId),
       { body: commands }
     );
